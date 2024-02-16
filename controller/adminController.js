6c57ea53fddd50c61ad5236bbf4b5b87171a4813
navya-krishnan <navyakrishnanplr2002@gmail.com>
2024-02-16 11:32:25 +0530

    message

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,10 @@
 const app = require('express')
 
+const credential={
+    email:"navyanaveen@gmail.com",
+    password:"naveen"
+};
+
 const getAdminLogin = (req,res)=>{
         res.render('adminLogin')
 }
